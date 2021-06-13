2b75ed3f06a753bea54f6f5b3b5f635c0cfb7d9e
hanifrfd <hanif.wrong@gmail.com>
2021-06-13 23:09:38 +0700

    give try catch to database request

--- src/scripts/data/restaurantDb.js
+++ src/scripts/data/restaurantDb.js
@@ -2,13 +2,21 @@ import API_ENDPOINT from '../globals/api-endpoint';
 
 class RestaurantDB {
   static async restaurantList() {
-    const response = await fetch(API_ENDPOINT.RESTAURANT_LIST);
-    return response.json();
+    try {
+      const response = await fetch(API_ENDPOINT.RESTAURANT_LIST);
+      return response.json();
+    } catch (err) {
+      return err;
+    }
   }
 
   static async detailRestaurant(id) {
-    const response = await fetch(API_ENDPOINT.DETAIL(id));
-    return response.json();
+    try {
+      const response = await fetch(API_ENDPOINT.DETAIL(id));
+      return response.json();
+    } catch (err) {
+      return err;
+    }
   }
 
   static async postReview(review) {
